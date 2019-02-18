3dc4d80d1e4d9189529655e5e65769c8ae4f0748
Tobbep1997 <tobbe.pogen@live.se>
2019-02-18 14:58:57 +0100

    PreAllocMemTest

--- MemoryManager/Test.cpp
+++ MemoryManager/Test.cpp
@@ -30,21 +30,28 @@ struct TestStruct
 };
 
 void RawPointerTest(std::vector<double> * allocTime, std::vector<double> * readTime, std::vector<double> * randomAccess, const unsigned int & testSize, const unsigned int & randomSeed);
+void PreHeapPointerTest(std::vector<double> * allocTime, std::vector<double> * readTime, std::vector<double> * randomAccess, const unsigned int & testSize, const unsigned int & randomSeed);
 void TestTestStruct(TestStruct* test);
 
 int main(int commands, char * arr[])
 {
-	//int commands = 3;
-	//const char * arr[] = { "test", "2", "output.txt" };
-
 	std::vector<double> alloc;
 	std::vector<double> read;
 	std::vector<double> random;
 
-	const int size = std::atoi(arr[1]);
-	RawPointerTest(&alloc, &read, &random, size, 420);
+	int size = 1024;
+	if (commands > 1)
+	size = std::atoi(arr[1]);
 
-	OutputClass::Output(arr[2], alloc, read, random);
+	RawPointerTest(&alloc, &read, &random, size, 420);
+	OutputClass::Output("Raw.txt", alloc, read, random);
+
+	alloc.clear();
+	read.clear();
+	random.clear();
+
+	PreHeapPointerTest(&alloc, &read, &random, size, 420);
+	OutputClass::Output("PreAlloc.txt", alloc, read, random);
 }
 
 void RawPointerTest(std::vector<double> * allocTime, std::vector<double> * readTime, std::vector<double> * randomAccess, const unsigned int & testSize, const unsigned int & randomSeed)
@@ -93,6 +100,50 @@ void RawPointerTest(std::vector<double> * allocTime, std::vector<double> * readT
 
 	delete[] allocTest;
 }
+void PreHeapPointerTest(std::vector<double> * allocTime, std::vector<double> * readTime, std::vector<double> * randomAccess, const unsigned int & testSize, const unsigned int & randomSeed)
+{
+	srand(randomSeed);
+	TestStruct ** allocTest = new TestStruct*[testSize];
+
+	DeltaTimer timer;
+
+	MemoryStack::Init(sizeof(TestStruct) * testSize);
+	for (size_t i = 0; i < testSize; i++)
+	{
+
+		timer.Init();
+		allocTest[i] = MemoryStack::AllocData<TestStruct>();
+
+		const double t = timer.GetDeltaTimeInSeconds();
+		allocTime->push_back(t);
+	}
+
+	for (size_t i = 0; i < testSize; i++)
+	{
+		timer.Init();
+
+		TestStruct * tmp = allocTest[i];
+		TestTestStruct(tmp);
+
+		const double t = timer.GetDeltaTimeInSeconds();
+		readTime->push_back(t);
+	}
+
+	for (size_t i = 0; i < testSize; i++)
+	{
+		timer.Init();
+
+		TestStruct * tmp = allocTest[rand() % testSize];
+		TestTestStruct(tmp);
+
+		const double t = timer.GetDeltaTimeInSeconds();
+		randomAccess->push_back(t);
+	}
+	   
+	delete[] allocTest;
+	MemoryStack::Release();
+	
+}
 
 void TestTestStruct(TestStruct* test)
 {
@@ -101,58 +152,3 @@ void TestTestStruct(TestStruct* test)
 	float z = test->z;
 	float w = test->w;
 }
-
-
-//int main(int commands, char * arr[])
-//{
-//	const size_t testSize = 0xfffffff;
-//	SetDbgFlag();
-//
-//
-//	DeltaTimer timer;
-//
-//	std::cout << "Default Alloc " << testSize << " Objects " << " Start" << std::endl;
-//
-//	TestStruct ** test = new TestStruct*[testSize];
-//
-//
-//	timer.Init();
-//	for (size_t i = 0; i < testSize; i++)
-//	{
-//		test[i] = new TestStruct();
-//	}
-//	for (size_t i = 0; i < testSize; i++)
-//	{
-//		delete test[i];
-//	}
-//
-//	const double DefaultTime = timer.GetDeltaTimeInSeconds();
-//	
-//	std::cout << "Default: " << DefaultTime << " Sec" << std::endl;
-//
-//	std::cout << "Pre Alloc " << testSize << " Objects " << " Start" << std::endl;
-//
-//	MemoryStack::Init(testSize * sizeof(TestStruct));
-//
-//	timer.Init();
-//	for (size_t i = 0; i < testSize; i++)
-//	{
-//		test[i] = MemoryStack::AllocData<TestStruct>();
-//		test[i]->x = static_cast<float>(i);
-//		test[i]->y = static_cast<float>(i);
-//		test[i]->z = static_cast<float>(i);
-//		test[i]->w = static_cast<float>(i);
-//	}
-//	for (size_t i = 0; i < testSize; i++)
-//	{
-//		MemoryStack::Free(test[i]);
-//	}
-//	const double MemTime = timer.GetDeltaTimeInSeconds();
-//
-//	delete[] test;
-//
-//	std::cout << "MemoryAllocator: " << MemTime << " Sec" << std::endl;
-//	MemoryStack::Release();
-//	std::cin.get();
-//	return 0;
-//}
